ed7de60f56986c18b092158d8d46c1442a8edc3c
Teza Alfian <tezaalfian2916@gmail.com>
2019-04-24 03:16:07 +0700

    viewnya

---
+++ src/koperasipetik/KoperasiPetik.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koperasipetik;
+
+/**
+ *
+ * @author Teza Alfian
+ */
+public class KoperasiPetik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
